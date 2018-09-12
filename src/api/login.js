54b9b2c67c4d0f1806de8660e9f89f0e661c87ac
zunz <zunz@zunz.net>
2018-09-12 16:28:26 +0800

    create dev_sun

--- src/api/login.js
+++ src/api/login.js
@@ -2,11 +2,11 @@ import request from '@/utils/request'
 
 export function login(username, password) {
   return request({
-    url: '/user/login',
+    url: '/admin/login',
     method: 'post',
     data: {
-      username,
-      password
+      'username' : username,
+      'password' : password,
     }
   })
 }
