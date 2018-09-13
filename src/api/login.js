1dfdb61a50d0ab6e600e4d3005081c91ee033f3d
zunz <zunz@zunz.net>
2018-09-13 14:03:07 +0800

    modify api url

--- src/api/login.js
+++ src/api/login.js
@@ -13,7 +13,7 @@ export function login(username, password) {
 
 export function getInfo(token) {
   return request({
-    url: '/user/info',
+    url: '/admin/info',
     method: 'get',
     params: { token }
   })
@@ -21,7 +21,7 @@ export function getInfo(token) {
 
 export function logout() {
   return request({
-    url: '/user/logout',
+    url: '/admin/logout',
     method: 'post'
   })
 }
